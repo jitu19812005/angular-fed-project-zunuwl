7108aa668ff73779b807520b4f58119a5d86791f
jitu19812005 <jitu19812005@gmail.com>
2022-07-26 12:06:38 +0800

    1 file modified

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,36 +1,35 @@
-import { InMemoryDbService } from 'angular-in-memory-web-api'; 
-import {Injectable} from '@angular/core';
-import {Employee} from './employee';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Injectable } from '@angular/core';
+import { Employee } from './employee';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
-  createDb(){
-    let employees=[
+  createDb() {
+    let employees = [
       {
-        id: 1, 
-        name: 'Ram', 
-        location:'Banglore', 
-        email: 'ram@gmail.com',
-        mobile:'9867512345'   
+        id: 1,
+        name: 'Jitendra',
+        location: 'Noida',
+        email: 'jitu@gmail.com',
+        mobile: '123121',
       },
       {
-        id: 2, 
-        name: 'Raj',
-        location:'Chennai', 
-        email: 'raj@mail.com', 
-        mobile: '7867534521'  
+        id: 2,
+        name: 'Akas',
+        location: 'Luckno',
+        email: 'Akas@mail.com',
+        mobile: '43534353453',
       },
-      { 
-        id: 3, 
-        name: 'Vinay',
-        location:'Pune', 
-        email: 'vinay@mail.com', 
-        mobile: '9975287450'  
-      }
-
+      {
+        id: 3,
+        name: 'Usha',
+        location: 'UP',
+        email: 'Usha@mail.com',
+        mobile: '4535456445',
+      },
     ];
-    return {employees};
+    return { employees };
   }
 }
